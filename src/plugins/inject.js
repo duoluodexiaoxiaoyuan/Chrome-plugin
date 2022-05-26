62e239feb1cd7642219afbdc95b522dc68d77762
duoluodexiaoxiaoyuan <1203125214@qq.com>
2022-05-26 20:52:10 +0800

    使用vue写谷歌插件

---
+++ src/plugins/inject.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: migor
+ * @Date: 2021-11-25 20:53:34
+ * @LastEditors: migor
+ * @LastEditTime: 2021-11-25 21:44:46
+ * @Description: file content
+ */
+console.log('this is my inject.js')
